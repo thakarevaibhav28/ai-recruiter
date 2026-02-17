911913dca158433081426804c00e21253dee2181
thakarevaibhav28 <vaibhav@vitric.in>
2026-02-17 10:30:07 +0530

    added assesment and bulk add endpoint

--- frontend/src/services/api/adminApi/adminApi.tsx
+++ frontend/src/services/api/adminApi/adminApi.tsx
@@ -6,7 +6,8 @@ class AdminApi {
 
     LOGIN: "/admin/login",
     ADD_CANDIDATE: "/api/candidate",
-    bulk_add_candidate:"/admin/interview/candidates/bulk"
+    bulk_add_candidate:"/admin/interview/candidates/bulk",
+    CREATE_ASESMENT_TEMPLATE:"/admin/assessment/template",  
   };
 
   // admin Login
@@ -21,6 +22,15 @@ class AdminApi {
     return api._post(this._url.ADD_CANDIDATE, data)
   }
 
+  // bulk add candidate
+  bulk_add_candidate(data:any){
+    return api._postFormData(this._url.bulk_add_candidate, data)
+  }
+ 
+  //create assessment template
+  createAssessmentTemplate(data:any){
+    return api._post(this._url.CREATE_ASESMENT_TEMPLATE, data)
+  }
 
 
 
