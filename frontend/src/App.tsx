25c873a80e38ddf80e6b05d44e9faf65c0c3146b
Vedant Khambadkar <vedantkhambadkar1234@gmail.com>
2026-02-18 19:04:53 +0530

    Merge Himanshu code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-// import UserRoutes from "./routes/user/UserRoutes";
+import UserRoutes from "./routes/UserRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
 function App() {
   return (
     <>
     <Routes>
       <Route path="/admin/*" element={<AdminRoutes />} />
-       {/* <Route path="/*" element={<UserRoutes />} /> */}
+       <Route path="/*" element={<UserRoutes />} />
 
     </Routes>
    
--- frontend/src/routes/UserRoutes.tsx
+++ frontend/src/routes/UserRoutes.tsx
@@ -1,33 +1,57 @@
 import { Route, Routes } from "react-router-dom";
+// import ProtectedRoute from "../routes/ProtectedRoute";
+import PageNotFound from "../common/PageNotFound";
 import ToastProvider from "../common/ToastProvider";
-function AdminRoutes() {
+import UserLogin from "../pages/user/UserLogin";
+import SystemCompatibilityCheck from "../pages/user/Systemcompatibilitycheck";
+import IdentityVerification from "../pages/user/Identityverification";
+import SelfieVerification from "../pages/user/Selfieverification";
+import InterviewInstructions from "../pages/user/Interviewinstructions";
+import MCQAssessment from "../pages/user/MCQAssessment";
+import AssessmentCompleted from "../pages/user/AssessmentCompleted";
+import SessionEnded from "../pages/user/SessionEnded";
+import VideoInterview from "../pages/user/VideoInterview";
+function App() {
   return (
     <>
       <ToastProvider />
-      <Route path="/user/login" element={<UserLogin />} />
-      <Route path="/user/system-check" element={<SystemCompatibilityCheck />} />
-      <Route
-        path="/user/Identity-verification"
-        element={<IdentityVerification />}
-      />
-      <Route
-        path="/user/selfie-verification"
-        element={<SelfieVerification />}
-      />
-      <Route
-        path="/user/interview-instruction"
-        element={<InterviewInstructions />}
-      />
-      <Route path="/user/mcq-assessment" element={<MCQAssessment />} />
-      <Route path="/user/video-interview" element={<VideoInterview />} />{" "}
-      <Route
-        path="/user/assessment-complete"
-        element={<AssessmentCompleted />}
-      />{" "}
-      <Route path="/user/session-end" element={<SessionEnded />} />
+      <Routes>
+        {/* User Routes */}
+        <Route path="/user/login" element={<UserLogin />} />
+
+        {/* <Route element={<ProtectedRoute redirectTo="/user/login" />}> */}
+          <Route
+            path="/user/system-check"
+            element={<SystemCompatibilityCheck />}
+          />
+          <Route
+            path="/user/Identity-verification"
+            element={<IdentityVerification />}
+          />
+          <Route
+            path="/user/selfie-verification"
+            element={<SelfieVerification />}
+          />
+          <Route
+            path="/user/interview-instruction"
+            element={<InterviewInstructions />}
+          />
+          <Route path="/user/mcq-assessment" element={<MCQAssessment />} />
+          <Route
+            path="/user/video-interview"
+            element={<VideoInterview />}
+          />{" "}
+          <Route
+            path="/user/assessment-complete"
+            element={<AssessmentCompleted />}
+          />{" "}
+          <Route path="/user/session-end" element={<SessionEnded />} />
+        {/* </Route> */}
+        {/* 404 Route */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
 
-export default AdminRoutes;
-
+export default App;
